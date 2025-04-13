fe333c9a22ad1dd5c12006ef841e61896368dbbd
Premapriya <premapriya1905@gmail.com>
2025-04-13 17:17:46 +0530

    commit9

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -4,7 +4,7 @@ const MessageSchema = new mongoose.Schema({
   sender: { type: String, required: true },
   receiver: { type: String, required: true, default: "Curatales" },
   message: { type: String, required: true },
-  audio: { type: String, default: null },
+  // audio: { type: String, default: null },
   isEdited: { type: Boolean, default: false },
   timestamp: { type: Date, default: Date.now },
 });
